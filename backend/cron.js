4fab63e47cd259bfaff3750a1a1e7cdeb773f931
Harsha70 <root@Harshavardhans-MacBook-Air.local>
2024-07-03 18:57:33 -0500

    cron added

---
+++ backend/cron.js
@@ -0,0 +1,20 @@
+import cron from "cron";
+import https from "https";
+
+const URL = "https://expense-tracker-ogbq.onrender.com";
+
+const job = new cron.CronJob("/14 * * * *", function () {
+  https
+    .get(URL, (res) => {
+      if (res.statusCode === 200) {
+        console.log("Get request sent successfully: ", res.statusCode);
+      } else {
+        console.log("Get request failed: ", res.statusCode);
+      }
+    })
+    .on("error", (err) => {
+      console.error("Error while bsending request: ", err);
+    });
+});
+
+export default job;
